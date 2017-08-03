91b1e5d8e6e4759364ebc4b7f97c9b6749d50f0b
SR Rajesh Kumar <rajeshux@gmail.com>
2017-08-03 13:45:19 +0530

    Functional test improved with a user case with lifecycle operations for browser

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_register_a_new_participant_info(self):
+        # Gaurav gets new entries on Symposium Participants.
+        # Gaurav fires his browser to fill each participant in the
+        # Mahati Portal
+        self.browser.get('http://localhost:8000')
+
+        # Gaurav notices the title of the browser, 'Mahati'
+        self.assertIn('Mahati', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
